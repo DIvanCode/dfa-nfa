73b33b054553bb185e6db49259ab4f6aa79bff4e
DIvanCode <divancode@gmail.com>
2023-10-05 20:42:39 +0300

    done task 4 (hw 4) - minimal dfa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,4 +3,5 @@ fun main() {
     println(task1NFA("input/nfa1.txt", "input/word1NFA.txt"))
     task2("input/nfa2.txt")
     println(task3("input/regExp3.txt", "input/word3.txt"))
+    task4("input/dfa4.txt")
 }
